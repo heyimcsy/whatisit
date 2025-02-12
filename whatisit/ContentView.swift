e37eed875febb1cfdba80fca69c67f5c13bb06e9
heyimcsy <cinnamon1995@naver.com>
2025-02-13 00:58:15 +0900

    Initial commit

--- whatisit/ContentView.swift
+++ whatisit/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    var name: String = "seoyoon"
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text(name)
+            Text("Hello, \(name)!")
         }
         .padding()
     }
