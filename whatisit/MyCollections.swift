1ab9d6513a79a3cef085102db4665423d9c05943
sychoi <cinnamon1995@naver.com>
2025-02-14 15:43:22 +0900

    push experiment

--- whatisit/MyCollections.swift
+++ whatisit/MyCollections.swift
@@ -11,15 +11,16 @@ import SwiftUI
 //vat가 변수
 struct MyCollections: View {
     let foods:[String] = [
-    "eggs", "bananas", "beans"
+    "eggs", "bananas", "beans",
     ]
     var jazzs: Set<String> = ["bidifi", "noeee", "sisididi"]
     var hiphop: Set<String> = ["noeee", "rap", "wow"]
-    var koEngDict: [String: String] = ["사과":"apple", "바나나":"banana"]
+    var koEngDict: [String: String] = ["사과":"apple", "바나나":"banana", "오렌지":"orange"]
     var body: some View {
         VStack {
             Text(koEngDict["사과"]!)
             Text(koEngDict["바나나"]!)
+            Text(koEngDict["오렌지"]!)
         }
         }
     }
