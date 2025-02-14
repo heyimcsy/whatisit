2bf621ee5ba33a0e4b2964e4403a7200f6cbdfa1
sychoi <cinnamon1995@naver.com>
2025-02-14 16:15:17 +0900

    learn about if and guard

---
+++ whatisit/Condition.swift
@@ -0,0 +1,18 @@
+//
+//  Condition.swift
+//  whatisit
+//
+//  Created by 최서윤 on 2/14/25.
+//
+
+import SwiftUI
+
+struct Condition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Condition()
+}
