35f0a3299e02c2b93fd01b98661833fd8bfc06a0
sychoi <cinnamon1995@naver.com>
2025-02-14 16:22:22 +0900

    learn about loop

---
+++ whatisit/Loop.swift
@@ -0,0 +1,18 @@
+//
+//  Loop.swift
+//  whatisit
+//
+//  Created by 최서윤 on 2/14/25.
+//
+
+import SwiftUI
+
+struct Loop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loop()
+}
